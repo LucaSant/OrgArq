b7e20c67379790fe052321a3cc0aa590d818418b
Antônio Medrado <40172891+APMedrado@users.noreply.github.com>
2022-05-19 21:20:54 -0300

    Complete upload of previous exercise

---
+++ exerc-1/registros.h
@@ -0,0 +1,11 @@
+// Antônio Medrado 10748389
+
+#include <stdio.h>
+
+void imprimeRegistro(FILE *f);
+int gravar(char filename[31]);
+int recuperar(char filename[31]);
+int recuperarRegistro(char filename[31]);
+
+void readline(char* string);
+void binarioNaTela(char *nomeArquivoBinario);
