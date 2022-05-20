15ba18a71e97149bb485db710457fd5df5f56b4e
Lucas Sant'Anna <lucas.hsantanna@gmail.com>
2022-05-20 17:52:27 -0300

    Crated funcionalidades.h and .c files, changed main.c including functions from this funcionalidades.c file

---
+++ trab-1/funcionalidades.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+FILE *createTable(char *file_type, char *input_filename);
+void select(char *file_type, char *iput_filename);
+void select_where(char *file_type, char *input_filename);
+void data_recovery(char *file_type, char *input_filename);
+
+
