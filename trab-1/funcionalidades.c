c9660b3c86b195899130350bbce4c0746b60b8c9
Lucas Sant'Anna <lucas.hsantanna@gmail.com>
2022-05-20 17:54:53 -0300

    fixing last commit

---
+++ trab-1/funcionalidades.c
@@ -0,0 +1,25 @@
+#include "funcionalidades.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+FILE* createTable(char *file_type, char *input_filename){
+    FILE *input_file = fopen(input_filename, "wb+");
+    char output_filename[31];
+    scanf("%s", output_filename);
+//...//
+    return output_file;
+}
+
+void select(char *file_type, char *input_filename){
+
+}
+
+void select_where(char *file_type, char *input_filename){
+
+}
+
+void data_recovery(char *file_type, char *input_filename){
+
+}
+
--- trab-1/main.c
+++ trab-1/main.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
-#include "cabecalho.h" // A DEFINIR
+#include "funcionalidades.h" // A DEFINIR
 
 int main() {
     int func, res;
-    scanf("%d", &func);
     char tipoArquivo[6], arquivoEntrada[31];
+    scanf("%d", &func);
     scanf("%s %s", tipoArquivo, arquivoEntrada);
 
     switch(func){
         case 1:
-            char arquivoSaida[31];
-            scanf("%s", arquivoSaida);
-            res = leituraGravacao(tipoArquivo, arquivoEntrada, arquivoSaida);
+            FILE *arquivoSaida = createTable(tipoArquivo, arquivoEntrada);
             break;
         case 2:
-            res = recuperaTudo(tipoArquivo, arquivoEntrada);
+            select(tipoArquivo, arquivoEntrada);
             break;
         case 3:
-            int n;
-            scanf("%d", &n);
-            res = recuperaBusca(tipoArquivo, arquivoEntrada, n);
+            select_where(tipoArquivo, arquivoEntrada);
             break;
         case 4:
-            int RRN;
-            scanf("%d", &RRN);
-            res = recuperaRegistro(tipoArquivo, arquivoEntrada, RRN);
+            data_recovery(tipoArquivo, arquivoEntrada);
             break;
         default:
             printf("Comando invalido! Deve estar entre 1 e 4\n");
