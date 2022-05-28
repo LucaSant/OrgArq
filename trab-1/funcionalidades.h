d9a9f917b4919892baf69b7ceb83f26c332fcba4
Antônio Medrado <40172891+APMedrado@users.noreply.github.com>
2022-05-28 15:26:27 -0300

    Delete trab-1 directory

--- trab-1/funcionalidades.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int create_table(char *file_type, char *input_filename);
-int select(char *file_type, char *iput_filename);
-int select_where(char *file_type, char *input_filename);
-int select_rrn(char *file_type, char *input_filename);
